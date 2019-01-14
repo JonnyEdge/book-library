d07b3ba45c43f52eb9485e4ef2aea2e3f01321d8
Jonny Edge <jonnyedge@outlook.com>
2019-01-14 20:13:17 +0000

    Refactored tests using helper functions and data factories.

--- tests/users.test.js
+++ tests/users.test.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const User = require('../src/models/usersmodel');
+const UserHelpers = require('../tests/helpers/users.helpers');
+const DataFactory = require('../tests/helpers/data-factory');
 
 describe('/users', () => {
   beforeEach((done) => {
@@ -10,41 +12,23 @@ describe('/users', () => {
 
   describe('POST /users', () => {
     it('creates a new user in the database', (done) => {
-      chai.request(server)
-        .post('/users')
-        .send({
-          firstName: 'Wimbledon',
-          lastName: 'Phuck',
-          email: 'wimbledon.phuck@aol.com',
-          password: 'phuckyeah',
+      const data = DataFactory.user();
+      UserHelpers.signUp(data)
+        .then((response) => {
+          expect(data.firstName).to.not.equal('');
+          expect(data.lastName).to.not.equal('');
+          expect(data.email).to.not.equal('');
+          expect(data.password).to.not.equal('');
+          expect(response.body).to.not.have.property('password');
+          done();
         })
-        .end((error, response) => {
-          expect(error).to.equal(null);
-          expect(response.status).to.equal(201);
-
-          User.findById(response.body._id, (error, user) => {
-            expect(error).to.equal(null);
-            expect(user.firstName).to.equal('Wimbledon');
-            expect(user.lastName).to.equal('Phuck');
-            expect(user.email).to.equal('wimbledon.phuck@aol.com');
-            // expect(user.password).to.not.equal('phuckyeah');
-            expect(user.password).to.have.lengthOf(60);
-            expect(response.body).to.not.have.property('password');
-            done();
-          });
-        });
+        .catch((error) => done(error));
     });
 
     it('throws an error when an invalid email address is entered', (done) => {
-      chai.request(server)
-        .post('/users')
-        .send({
-          firstName: 'Wimbledon',
-          lastName: 'Phuck',
-          email: 'wimbledon.phuckataoldotcom',
-          password: 'phuckyeah',
-        })
-        .end((error, response) => {
+      const data = DataFactory.user({ email: 'wimbledonphuckataoldotcom' });
+      UserHelpers.signUp(data)
+        .then((response) => {
           expect(response.status).to.equal(400);
           expect(response.body.errors.email).to.equal('Invalid email address entered.');
 
@@ -53,19 +37,14 @@ describe('/users', () => {
           });
 
           done();
-        });
+        })
+        .catch((error) => done(error));
     });
 
     it('throws an error when an invalid password is entered', (done) => {
-      chai.request(server)
-        .post('/users')
-        .send({
-          firstName: 'Wimbledon',
-          lastName: 'Phuck',
-          email: 'wimbledon.phuck@aol.com',
-          password: 'phuckno',
-        })
-        .end((error, response) => {
+      const data = DataFactory.user({ password: 'phuckno' });
+      UserHelpers.signUp(data)
+        .then((response) => {
           expect(response.status).to.equal(400);
           expect(response.body.errors.password).to.equal('Password must be at least 8 characters long.');
 
@@ -74,7 +53,8 @@ describe('/users', () => {
           });
 
           done();
-        });
+        })
+        .catch((error) => done(error));
     });
   });
 });
